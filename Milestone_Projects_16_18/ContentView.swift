7d4b0061f11dbcf6f331b94638a3c059005a34a5
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-01-20 13:51:47 +0800

    set ui for starter of project milestone

--- Milestone_Projects_16_18/ContentView.swift
+++ Milestone_Projects_16_18/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var diceRolls = [Dice]()
+    @State private var showingDetailView = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                List(diceRolls) {
+                    Text("\($0.number)")
+                }
+                
+                
+                Button("Roll dice") {
+                    let randomDice = Int.random(in: 1...50)
+                    let dice = Dice(number: randomDice, diceAmount: 1, faceAmount: 1)
+                    diceRolls.append(dice)
+                }
+            }
+            .navigationTitle("Dice rolls")
+        }
+        
     }
 }
 
