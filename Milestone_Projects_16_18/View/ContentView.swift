79d1414c3e837f43f5f7328d8fcebec446eff06f
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-01-20 16:20:44 +0800

    set stack and image at the bottom

---
+++ Milestone_Projects_16_18/View/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  Milestone_Projects_16_18
+//
+//  Created by Giovanni Gaffé on 2022/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var diceRolls = [Dice]()
+    @State private var showingDetailView = false
+    @State private var isFirstEdit = false
+    var body: some View {
+        NavigationView {
+            VStack {
+                List(diceRolls) {
+                    Text("\($0.TotalAmountDice)")
+                }
+                
+                
+                // MARK: - Buttons
+
+                HStack {
+                    Button(role: .cancel) {
+                        startRollingDices()
+                    } label: {
+                        Text("Roll dices")
+                        Image(systemName: "dice.fill")
+                    }
+                    .disabled(isFirstEdit ? false : true)
+                    
+                    
+                    
+                    Button(role: .cancel) {
+                        isFirstEdit.toggle()
+                        showingDetailView.toggle()
+                    } label: {
+                        Text("Change dices")
+                        Image(systemName: "pencil")
+                    }
+                }
+                
+            }
+            .sheet(isPresented: $showingDetailView, content: {
+                DiceDetailView()
+            })
+            .navigationTitle("Dice rolls")
+        }
+        
+    }
+    
+    func startRollingDices() {
+        let randomDice = Int.random(in: 1...50)
+        let dice = Dice(TotalAmountDice: randomDice, diceAmount: 1, faceAmount: 1)
+        diceRolls.append(dice)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
