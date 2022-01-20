7852701f1b7d453a6a3819233c9d7652e5eb8a23
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-01-20 13:52:23 +0800

    set model with some information about dice

---
+++ Milestone_Projects_16_18/Model/Dice.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  Milestone_Projects_16_18
+//
+//  Created by Giovanni Gaffé on 2022/1/20.
+//
+
+import Foundation
+
+struct Dice: Identifiable {
+    let id = UUID()
+    let number: Int
+    let diceAmount: Int
+    let faceAmount: Int
+
+}
